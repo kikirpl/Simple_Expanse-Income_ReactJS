d65d8602e0489427c9db15ece7d6a2f7f133e03d
kikirpl <rizkyibrohim88@gmail.com>
2024-08-05 13:11:07 +0700

    simple

---
+++ src/component/InputForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const InputForm = () => {
+  return (
+    <div className="input-form">
+      <div className="form-item">
+        <label>Transaction Name :</label>
+        <input type="text"></input>
+      </div>
+    </div>
+  );
+};
+export default InputForm;
---
+++ src/component/Title.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div>
+      <h1>Income and Expense</h1>
+    </div>
+  );
+};
+
+export default Title;
